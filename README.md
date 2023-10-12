39b8f7dcb319a01c3a110b982009f790d6245cf7
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-10-12 20:30:27 +0200

    Slot Machine Project, step 1

---
+++ README.md
@@ -0,0 +1,7 @@
+[X] deposit some money
+[ ] determine number of lines to bet on
+[ ] collect the bet amount
+[ ] spin the slot machine
+[ ] check if the user won
+[ ] give the user their winnings
+[ ] play agian ?
---
+++ slot.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')();
+
+// deposit some money
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt('Enter a deposit amount: ');
+        const numberDepositAmount = parseFloat(depositAmount);
+    
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log('Not a valid amount, try again.');
+        } else {
+            return numberDepositAmount;
+        }
+    }
+}
+
+const depositAmount = deposit();
+console.log(depositAmount);
