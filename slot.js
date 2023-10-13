544e25ac4067bbb75489222dfe00d589da6854b6
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-10-13 22:41:21 +0200

    Spin the slot machine

--- slot.js
+++ slot.js
@@ -1,5 +1,22 @@
 const prompt = require('prompt-sync')();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+
+const SYMBOL_VALUE = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
 // deposit some money
 const deposit = () => {
     while (true) {
@@ -44,6 +61,33 @@ const getBet = (balance, betLines) => {
     }
 }
 // =========================
+
+// spin the slot machine
+const spin = () => {
+    const symbols = [];
+
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    
+    const reels = [[], [], []];
+
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selecterSymbol = reelSymbols[randomIndex];
+            reels[i].push(selecterSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+}
+
+const reels = spin()
+console.log(reels)
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
